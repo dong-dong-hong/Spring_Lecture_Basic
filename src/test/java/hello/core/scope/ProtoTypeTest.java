2822c47636006691446a3881aa72a229886a322b
동동 <87763201+dong-dong-hong@users.noreply.github.com>
2023-03-14 18:32:00 +0900

    commit

---
+++ src/test/java/hello/core/scope/ProtoTypeTest.java
@@ -0,0 +1,52 @@
+package hello.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ProtoTypeTest {
+
+    @Test
+    void ProtoTypeBeanTest(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ProtoTypeBean.class);
+        System.out.println("find prototypeBean1");
+        ProtoTypeBean protoTypeBean1 = ac.getBean(ProtoTypeBean.class);
+        System.out.println("find prototypeBean2");
+        ProtoTypeBean protoTypeBean2 = ac.getBean(ProtoTypeBean.class);
+        System.out.println("protoTypeBean1 = " + protoTypeBean1);
+        System.out.println("protoTypeBean2 = " + protoTypeBean2);
+        assertThat(protoTypeBean1).isNotSameAs(protoTypeBean2);
+
+//        protoTypeBean1.destory();
+//        protoTypeBean2.destory();
+
+        ac.close();
+    }
+    @Scope("prototype")
+    static class ProtoTypeBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("ProtoTypeBean.init");
+        }
+        @PreDestroy
+        public void destory() {
+            System.out.println("ProtoTypeBean.destory");
+        }
+    }
+}
+// 스프링 빈은 스프링 컨테이너 생성 시점에 초기화 메서드가 실행 되지만, 프로토타입 스코프 빈은 스프링 컨테이너에서 빈을 조회할 때 생성되고, 초기화 메서드도 실행된다.
+// 프로토타입 빈을 2번 조회했으므르 완전히 다른 스프링 빈이 생성되고, 초기화도 2번 실행된 것을 확인할 수 있다.
+// 싱글톤 컨테이너가 생성과 의존관계 주입 그리고 초기화까지만 관여하고, 더는 관리하지 않는다.
+// 따라서 프로토타입 빈은 스프링 컨테이너가 종료될 때 @PreDestory같은 종료 메서드가 전혀 실행되지 않는다.
+
+// 프로토타입 빈의 특징 정리
+// 스프링 컨테이너에 요청할 때 마다 새로 생성된다.
+// 스프링 컨테이너는 프로토타입 빈의 생성과 의존관계 주입 그리고 초기화까지만 관여한다.
+// 종료 메서드가 호출되지 않는다.
+// 그래서 프로토타입 빈은 프로토타입 빈을 조회한 클라이언트가 관리해야 한다. 종료 메서드에 대한 호출도 클라이언트가 직접 해야한다.
